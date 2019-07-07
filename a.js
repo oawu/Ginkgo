7ab529d77fae768806c47e5ef62e5718c9bf6838
OA Wu <comdan66@gmail.com>
2019-07-08 00:12:48 +0800

    Fix bug

---
+++ a.js
@@ -0,0 +1,61 @@
+{
+    "advs": [
+        {
+            "id": 1,
+            "title": "廣告標題",
+            "content": "廣告內容",
+            "status": "pass",
+            "enable": false,
+            "isLike": false,
+            "dblAvatar": true,
+            "createAt": "2018-12-31 12:59:59",
+            "user": {
+                "id": 1,
+                "name": "OA",
+                "avatar": {
+                    "ori": "https://cdn.adpost.com.tw/adpost/other/api_doc/default.jpg",
+                    "w100": "https://cdn.adpost.com.tw/adpost/other/api_doc/default.jpg",
+                    "c120x120": "https://cdn.adpost.com.tw/adpost/other/api_doc/default.jpg"
+                }
+            },
+            "brand": {
+                "id": 7,
+                "name": "品牌名稱_修改",
+                "avatar": {
+                    "ori": "https://cdn.adpost.com.tw/adpost/other/api_doc/default.jpg",
+                    "w100": "https://cdn.adpost.com.tw/adpost/other/api_doc/default.jpg",
+                    "c120x120": "https://cdn.adpost.com.tw/adpost/other/api_doc/default.jpg"
+                }
+            },
+            "covers": [
+                {
+                    "id": 1,
+                    "type": 'video',
+                    "pic": {
+                        "ori": "https://cdn.adpost.com.tw/adpost/other/api_doc/default.jpg",
+                        "w100": "https://cdn.adpost.com.tw/adpost/other/api_doc/default.jpg",
+                        "w1440": "https://cdn.adpost.com.tw/adpost/other/api_doc/default.jpg",
+                        "c1200x630": "https://cdn.adpost.com.tw/adpost/other/api_doc/default.jpg"
+                    },
+                    "duration": 100
+                },
+            ],
+            "cnt": {
+                "pv": 0,
+                "upv": 0,
+                "like": 0,
+                "message": 0
+            },
+            "sellPrice": 100,
+            "brandPrice": 30000,
+            "product": {
+                "id": 1,
+                "title": "商品標題",
+                "rule": "投稿規則",
+                "content": "商品內容"
+                "enable": true,
+                "isDelete": false
+            }
+        }
+    ]
+}
